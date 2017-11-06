0aeba0b2501449b50eb64cc126800fea4ff65942
Larney11 <larharneycomputing@gmail.com>
2017-11-06 21:20:37 +0000

    Add name to artist.

--- GigFeed/Controllers/AccountController.cs
+++ GigFeed/Controllers/AccountController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using GigFeed.Models;
+using GigFeed.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using GigFeed.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace GigFeed.Controllers
 {
@@ -151,7 +149,11 @@ namespace GigFeed.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                { UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
