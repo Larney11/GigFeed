8800443733657b45d63ebd30a28ecaf8dc65ac34
Larney11 <larharneycomputing@gmail.com>
2018-08-13 17:52:21 +0100

    User can now use toggle button for attending and not attending an event

--- GigFeed/ViewModels/GigsViewModel.cs
+++ GigFeed/ViewModels/GigsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GigFeed.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GigFeed.ViewModels
 {
@@ -9,5 +10,6 @@ namespace GigFeed.ViewModels
         public bool ShowActions { get; set; }
         public string Heading { get; set; }
         public string SearchTerm { get; set; }
+        public ILookup<int, Attendance> Attendances { get; set; }
     }
 }
